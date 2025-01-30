eb3f9c6537299c6af1adcce809e0f790697609e5
Mellouli, Ismail (IntlAssoc) <ismail.mellouli@nist.gov>
2025-01-30 14:00:04 -0500

    ci/cd test

---
+++ codeset-adapter/buildScript.sh
@@ -0,0 +1,1 @@
+sudo mvn clean install -DskipTests
--- infrastructure/containers/apps/start.sh
+++ infrastructure/containers/apps/start.sh
@@ -1,1 +1,2 @@
-docker compose -f docker-compose.yml up --build  -d
+# docker compose -f docker-compose.yml up --build  -d
+COMPOSE_PROFILES=prod docker compose -f docker-compose.yml up -d --pull always
